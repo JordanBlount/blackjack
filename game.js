55481174ab44b283e929be83e8e0014a655824e4
Jordan Blount <77266395+JordanBlount@users.noreply.github.com>
2021-08-13 13:29:17 -0400

    Initial logic in javascript

---
+++ game.js
@@ -0,0 +1,13 @@
+//let suit = ["♣", "♦", "♥","♠"]
+let suit = ["clubs", "diamonds", "hearts", "spages"]
+let values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+
+class Card {
+    constructor(suit, value, points) {
+        this.suit = suit;
+        this.value = value;
+        this.points = points;
+    }
+}
+
+
--- links.md
+++ links.md
@@ -1,2 +1,11 @@
 https://developer.mozilla.org/en-US/docs/Web/CSS/box-shadow
 
+https://boardgames.stackexchange.com/questions/31423/can-different-aces-have-different-values
+
+https://www.youtube.com/watch?v=zuZqw12MrVI
+
+TODO USE
+
+https://animate.style/
+https://animejs.com/
+https://www.youtube.com/watch?v=g7WnZ9hxUak
