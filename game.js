36ce161eb4d08e48a4e0f60bfe45a2fe61d8a4d9
Jordan Blount <77266395+JordanBlount@users.noreply.github.com>
2021-08-13 14:14:42 -0400

    Changed up the Deck class

--- game.js
+++ game.js
@@ -20,42 +20,98 @@ class Card {
 
 class Deck {
 
-    // We can either fill a deck with a set of cards passed into it
-    // or it will automatically be created for us.
     constructor(cards) {
         this.cards = cards;
     }
 
     shuffle() {
-    
-    }
-
-    create() {
-        let cardsArray = [];
-
-        cardsArray = suit.flatMap(suit => {
-            return values.map(value => {
-                return new Card(suit, value, []);
-            })
-        });
-    
-        // TODO: I may be able to remove this logic because I can change the
-        // way that my game works to not have to do it this way.
-        cardsArray.map(card => {
-            if(card.value === "J" || card.value === "Q" || card.value === "K") {
-                card.points.push(10);
-            } else if(card.value === "A") {
-                card.points.push(1, 11);
-            } else {
-                card.points.push(parseInt(card.value));
-            }
-        });
-        this.cards = cardsArray;    
+        
     }
 }
 
-let deck = new Deck();
-deck.create();
+const createDeck = () => {
+    let cards = [];
 
+    cards = suit.flatMap(suit => {
+        return values.map(value => {
+            return new Card(suit, value, []);
+        })
+    });
+
+    // TODO: I may be able to remove this logic because I can change the
+    // way that my game works to not have to do it this way.
+    cards.map(card => {
+        if(card.value === "J" || card.value === "Q" || card.value === "K") {
+            card.points.push(10);
+        } else if(card.value === "A") {
+            card.points.push(1, 11);
+        } else {
+            card.points.push(parseInt(card.value));
+        }
+    });
+    return new Deck(cards);
+}
+
+let deck = createDeck();
 console.log(deck.cards[12]);
 console.log(deck.cards[12].isAce());
+
+class Player {
+
+    // We want to give the player a name, the current cards in their hand,
+    // and what is their turn which I may give them based on their current
+    // position in the players' array (if I add multiple players)
+    constructor(name, hand, isDealer) {
+        this.name = name;
+        this.hand = hand;
+    }
+
+    isDealer() {
+        return this.isDealer;
+    }
+
+    handTotal() {
+        // Two different totals. 
+        let total = 0;
+        
+        // TODO: Get the value of all the cards except for aces
+        //       Determine the value of the player's hand. If that value
+        //       + the value of the amount of Aces (e.g., if there are
+        //       2 aces: 11 + 1 or 1 + 1 if the first condition would give
+        //       the player more than 21)
+
+        // Solutions: Use an array to remove all aces and calculate those 
+        // values
+
+        let aces = this.amountOfAces();
+        if(aces > 0) {
+            if((total + 11) > 21) {
+                total + aces;
+            } else {
+                total += 11;
+                total += aces - 1; //This accomdates for the value of Aces
+            }
+        }
+        return total; 
+    }
+
+    amountOfAces() {
+        return this.hand.reduce((total, card) => {
+            if(card.value === "A") {
+                total += 1;
+            }
+        });
+    }
+
+    hasAces() {
+        return this.amountOfAces() > 0;
+    }
+
+    isBusted() {
+        return this.handTotal() >= 21;
+    }
+
+    addCards(cards) {
+        return this.hand.push(cards);
+    }
+}
--- links.md
+++ links.md
@@ -2,6 +2,8 @@ https://developer.mozilla.org/en-US/docs/Web/CSS/box-shadow
 
 https://boardgames.stackexchange.com/questions/31423/can-different-aces-have-different-values
 
+https://en.wikipedia.org/wiki/Standard_52-card_deck#:~:text=A%20standard%2052%2Dcard%20deck,court%20cards%20(face%20cards).
+
 https://www.youtube.com/watch?v=zuZqw12MrVI
 
 TODO USE
